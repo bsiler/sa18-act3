973a79217e6315488db9d72d68aed661a03be86a
Benjamin Siler <bengsiler@gmail.com>
2024-04-23 03:18:48 -0500

    Added show page routing

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('products/', views.product_index, name='product_index'),
+    path('products/<int:id>', views.product_show, name='product_show'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from .models import Product
+
 def product_index(request): 
     products = Product.objects.all()
     return render(request, 'products/index.html', {'products': products})
+
+def product_show(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'products/show.html', {'product': product})
